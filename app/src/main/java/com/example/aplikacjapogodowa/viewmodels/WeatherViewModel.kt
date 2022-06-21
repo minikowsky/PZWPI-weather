35a3a16339ad97f4459b8c1e0e54fd2740236f07
minikowsky <marszalek.dominik@gmail.com>
2022-06-21 19:24:09 +0200

    add weather fragment and viewmodel

---
+++ app/src/main/java/com/example/aplikacjapogodowa/viewmodels/WeatherViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.aplikacjapogodowa.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.aplikacjapogodowa.model.WeatherAPIResponse
+import com.example.aplikacjapogodowa.network.WeatherRepository
+import kotlinx.coroutines.launch
+
+private const val DEFAULT_CITY: String = "GLIWICE"
+
+class WeatherViewModel : ViewModel() {
+    private val _weather: MutableLiveData<WeatherAPIResponse> = MutableLiveData()
+    val weather: LiveData<WeatherAPIResponse>
+        get() {
+            return _weather
+        }
+
+    fun getWeather(unit: String, city: String = DEFAULT_CITY) {
+        viewModelScope.launch {
+            val w = WeatherRepository.get(unit, city)
+            if(w != null) {
+                _weather.value = w!!
+            }
+        }
+    }
+
+    fun getWeatherByLocation(unit: String, latitude: Double?, longitude: Double?) {
+        if(latitude == null || longitude == null) {
+            getWeather(unit)
+        }
+
+        viewModelScope.launch {
+            val w = WeatherRepository.getByLocation(unit, latitude.toString(), longitude.toString())
+            if(w != null) {
+                _weather.value = w!!
+            }
+        }
+    }
+}
